c6ad315eae1c2a29642fb3fd1228dadf6a961599
Adarsh Ambastha <72976932+Adarsh-Ambastha@users.noreply.github.com>
2022-12-19 15:01:07 +0530

    Update WhatsApp_App.py

--- WhatsApp_App.py
+++ WhatsApp_App.py
@@ -8,7 +8,7 @@ import preprocessor,helper
 import re
 import campusxpreprocessor
 #The Main Heading of the project on Side bar 
-st.sidebar.title("WhatsApp Char Analyzer")
+st.sidebar.title("WhatsApp Chat Analyzer")
 pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 #Uploding the chat from the device in txt formate only
 uploaded_file = st.sidebar.file_uploader("Choose a file",type=['txt'])
